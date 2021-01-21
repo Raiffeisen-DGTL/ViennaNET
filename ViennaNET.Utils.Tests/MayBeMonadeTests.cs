a8c524862796a887b888f0b598e81aa008bffb5f
SergeyO <excell@yandex.ru>
2021-01-22 01:34:21 +0300

    ViennaNET.Utils - add nullable reference types feature - part2

--- ViennaNET.Utils.Tests/MayBeMonadeTests.cs
+++ ViennaNET.Utils.Tests/MayBeMonadeTests.cs
@@ -15,7 +15,7 @@ namespace ViennaNET.Utils.Tests
 
       public long Value { get; }
     }
-    
+
     [Test]
     public void ThrowIf_WhenPredicateIsFalse_ShouldReturnValue()
     {
@@ -124,10 +124,10 @@ namespace ViennaNET.Utils.Tests
       // Arrange
       var failureValue = 0xFA1L;
       Foo[]? collection = null;
-      
+
       // Act
       var result = collection.At(5, f => f.Value * 10, failureValue);
-      
+
       // Assert
       Assert.That(result, Is.EqualTo(failureValue));
     }
@@ -138,10 +138,10 @@ namespace ViennaNET.Utils.Tests
       // Arrange
       var failureValue = 0xFA1L;
       var collection = new[] {new Foo(1), new Foo(2), new Foo(3)};
-      
+
       // Act
       var result = collection.At(collection.Length, f => f.Value * 10, failureValue);
-      
+
       // Assert
       Assert.That(result, Is.EqualTo(failureValue));
     }
@@ -152,14 +152,14 @@ namespace ViennaNET.Utils.Tests
       // Arrange
       var failureValue = 0xFA1L;
       var collection = new[] {new Foo(1), new Foo(2), new Foo(3)};
-      
+
       // Act
       var result = collection.At(1, f => f.Value * 10, failureValue);
-      
+
       // Assert
       Assert.That(result, Is.EqualTo(20));
     }
-    
+
     [Test]
     public void Do_WhenObjIsNull_ShouldNotInvokeActionAndReturnNull()
     {
@@ -167,14 +167,14 @@ namespace ViennaNET.Utils.Tests
       int? objStruct = null;
       var called = false;
 
-      var result = obj.Do(o => called = true);  
+      var result = obj.Do(o => called = true);
       var resultStruct = objStruct.Do(o => called = true);
-      
+
       Assert.That(called, Is.False);
       Assert.That(result, Is.Null);
       Assert.That(resultStruct, Is.Null);
     }
-    
+
     [Test]
     public void Do_ShouldNotInvokeActionWithObjectAndReturnNull()
     {
@@ -186,7 +186,7 @@ namespace ViennaNET.Utils.Tests
       {
         Assert.That(o, Is.SameAs(obj));
         called = true;
-      });  
+      });
       Assert.That(result, Is.SameAs(obj));
       Assert.That(called, Is.True);
 
@@ -205,51 +205,51 @@ namespace ViennaNET.Utils.Tests
     {
       Foo? fooNull = null;
       Foo foo = new Foo(123);
-      
+
       Assert.IsNull(fooNull.If(f => f.Value == 0x404));
       Assert.IsNull(foo.If(f => f.Value == 0x404));
-      
+
       Assert.That(foo.If(f => f.Value == 123), Is.SameAs(foo));
     }
-    
+
     [Test]
     public void IfNot_Object_ShouldReturnCorrectResult()
     {
       Foo? fooNull = null;
       Foo foo = new Foo(0x404);
-      
+
       Assert.IsNull(fooNull.IfNot(f => f.Value == 0x404));
       Assert.IsNull(foo.IfNot(f => f.Value == 0x404));
-      
+
       Assert.That(foo.IfNot(f => f.Value == 123), Is.SameAs(foo));
     }
-    
+
     [Test]
     public void If_Struct_ShouldReturnCorrectResult()
     {
       int? valueNull = null;
       int? value = 123;
-      
+
       Assert.IsNull(valueNull.If(f => false));
       Assert.IsNull(value.If(f => false));
-      
+
       Assert.That(value.If(f => true), Is.EqualTo(value));
     }
-    
+
     [Test]
     public void IfNot_Struct_ShouldReturnCorrectResult()
     {
       int? valueNull = null;
       int? value = 123;
-      
+
       Assert.IsNull(valueNull.IfNot(f => true));
       Assert.IsNull(value.IfNot(f => true));
-      
+
       Assert.That(value.IfNot(f => false), Is.EqualTo(value));
     }
 
     [Test]
-    public void Return_ShouldReturnInputIfItsNotNull()
+    public void Return_ShouldReturnInputEvaluationIfItsNotNull()
     {
       var result = new object();
       Foo? fooNull = null;
@@ -262,6 +262,133 @@ namespace ViennaNET.Utils.Tests
 
       Assert.That(foo.Return(f => result), Is.EqualTo(result));
       Assert.That(value.Return(f => result), Is.EqualTo(result));
+    }
+
+    [Test]
+    public void ReturnSuccess_ShouldReturnIfInputIsNotNull()
+    {
+      Foo? fooNull = null;
+      var foo = new Foo(123);
+
+      Assert.That(fooNull.ReturnSuccess(), Is.False);
+      Assert.That(foo.ReturnSuccess(), Is.True);
+    }
+
+    [Test]
+    public void IfNull_ShouldCallActionIfInputIsNotNull()
+    {
+      Foo? fooNull = null;
+      var foo = new Foo(123);
+
+      var called = false;
+      void SetCalledTrue() => called = true;
+      Assert.That(foo.IfNull(SetCalledTrue), Is.SameAs(foo));
+      Assert.That(called, Is.False);
+
+      Assert.That(fooNull.IfNull(SetCalledTrue), Is.Null);
+      Assert.That(called, Is.True);
+    }
+
+    [Test]
+    public void ThrowIfNull_ShouldThrowIfInputIsNull()
+    {
+      Foo? fooNull = null;
+      var foo = new Foo(123);
+
+      Assert.That(foo.ThrowIfNull(nameof(foo)), Is.SameAs(foo));
+
+      var exception = Assert.Throws<ArgumentNullException>(() => fooNull.ThrowIfNull(nameof(foo)));
+      Assert.That(exception.ParamName, Is.EqualTo(nameof(foo)));
+    }
+
+    [Test]
+    public void ThrowIfNullOrEmpty_ShouldThrowIfInputIsNullOrEmptyString()
+    {
+      string? nullString = null;
+      string emptyString = string.Empty;
+      string someString = "foo";
+
+      Assert.That(someString.ThrowIfNull(nameof(someString)), Is.SameAs(someString));
+
+      var exception = Assert.Throws<ArgumentNullException>(() => nullString.ThrowIfNullOrEmpty(nameof(nullString)));
+      Assert.That(exception.ParamName, Is.EqualTo(nameof(nullString)));
+
+      exception = Assert.Throws<ArgumentNullException>(() => emptyString.ThrowIfNullOrEmpty(nameof(emptyString)));
+      Assert.That(exception.ParamName, Is.EqualTo(nameof(emptyString)));
+    }
+
+    [Test]
+    public void ThrowIfNullOrEmpty_ShouldThrowSpecifiedExceptionIfInputIsNullOrEmptyString()
+    {
+      string? nullString = null;
+      string emptyString = string.Empty;
+      string someString = "foo";
+      var expectedException = new ArgumentNullException("null or empty");
+
+      Assert.That(someString.ThrowIfNull(nameof(someString)), Is.SameAs(someString));
+
+      var exception = Assert.Throws<ArgumentNullException>(() => nullString.ThrowIfNullOrEmpty(expectedException));
+      Assert.That(exception, Is.SameAs(expectedException));
+
+      exception = Assert.Throws<ArgumentNullException>(() => emptyString.ThrowIfNullOrEmpty(expectedException));
+      Assert.That(exception, Is.SameAs(expectedException));
+    }
+
+    [Test]
+    public void ThrowIfNullOrWhitespace_ShouldThrowIfInputIsNullOrEmptyString()
+    {
+      string? nullString = null;
+      string emptyString = string.Empty;
+      string whitespaceString = "\n";
+      string someString = "foo";
+
+      Assert.That(someString.ThrowIfNull(nameof(someString)), Is.SameAs(someString));
+
+      var exception =
+        Assert.Throws<ArgumentNullException>(() => nullString.ThrowIfNullOrWhiteSpace(nameof(nullString)));
+      Assert.That(exception.ParamName, Is.EqualTo(nameof(nullString)));
+
+      exception = Assert.Throws<ArgumentNullException>(() => emptyString.ThrowIfNullOrWhiteSpace(nameof(emptyString)));
+      Assert.That(exception.ParamName, Is.EqualTo(nameof(emptyString)));
+
+      exception = Assert.Throws<ArgumentNullException>(() =>
+        whitespaceString.ThrowIfNullOrWhiteSpace(nameof(whitespaceString)));
+      Assert.That(exception.ParamName, Is.EqualTo(nameof(whitespaceString)));
+    }
+
+    [Test]
+    public void ThrowIfNullOrWhiteSpace_ShouldThrowSpecifiedExceptionIfInputIsNullOrEmptyOrWhitespaceString()
+    {
+      string? nullString = null;
+      string emptyString = string.Empty;
+      string whitespaceString = "\n";
+      string someString = "foo";
+      var expectedException = new ArgumentNullException("null or empty");
+
+      Assert.That(someString.ThrowIfNull(nameof(someString)), Is.SameAs(someString));
+
+      var exception = Assert.Throws<ArgumentNullException>(() => nullString.ThrowIfNullOrWhiteSpace(expectedException));
+      Assert.That(exception, Is.SameAs(expectedException));
+
+      exception = Assert.Throws<ArgumentNullException>(() => emptyString.ThrowIfNullOrWhiteSpace(expectedException));
+      Assert.That(exception, Is.SameAs(expectedException));
+
+      exception = Assert.Throws<ArgumentNullException>(
+        () => whitespaceString.ThrowIfNullOrWhiteSpace(expectedException));
+      Assert.That(exception, Is.SameAs(expectedException));
+    }
+
+    [Test]
+    public void ThrowIfNull_WhenInputIsNull_ShouldThrowSpecifiedException()
+    {
+      Foo? fooNull = null;
+      var foo = new Foo(123);
+      var expectedException = new ArgumentNullException("null or empty");
+
+      Assert.That(foo.ThrowIfNull(nameof(foo)), Is.SameAs(foo));
+
+      var exception = Assert.Throws<ArgumentNullException>(() => fooNull.ThrowIfNull(expectedException));
+      Assert.That(exception, Is.SameAs(expectedException));
     }
   }
 }
