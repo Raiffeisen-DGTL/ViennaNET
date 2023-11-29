49091329c4c0488cc89f45c3bcddd8d94eb9ce38
KULBIDA Pavel <Pavel.A.KULBIDA@raiffeisen.ru>
2023-11-29 22:13:20 +0600

    fix build

---
+++ src/ViennaNET.Orm/Log/NHibernateToMicrosoftLoggerFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NHibernate;
+using ILoggerFactory = Microsoft.Extensions.Logging.ILoggerFactory;
+
+namespace ViennaNET.Orm.Log
+{
+  internal class NHibernateToMicrosoftLoggerFactory : INHibernateLoggerFactory
+  {
+    private readonly ILoggerFactory _loggerFactory;
+
+    public NHibernateToMicrosoftLoggerFactory(ILoggerFactory loggerFactory)
+    {
+      _loggerFactory = loggerFactory ?? throw new ArgumentNullException(nameof(loggerFactory));
+    }
+
+    public INHibernateLogger LoggerFor(string keyName)
+    {
+      var msLogger = _loggerFactory.CreateLogger(keyName);
+      return new NHibernateToMicrosoftLoggerAdapter(msLogger);
+    }
+
+    public INHibernateLogger LoggerFor(Type type)
+    {
+      return LoggerFor(type.FullName);
+    }
+  }
+}
